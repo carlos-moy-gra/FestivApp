3b8f7f01e8c8c0901d8741a45d09eeefc3e7312b
ivanmaz9 <ivanmartinezm09@gmail.com>
2020-03-04 11:38:06 +0100

    Un cambio en strings xml y añadida activity generos

---
+++ Festivapp/app/src/main/java/com/example/festivapp/ui/GenerosActivity.java
@@ -0,0 +1,16 @@
+package com.example.festivapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.festivapp.R;
+
+public class GenerosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generos);
+    }
+}
