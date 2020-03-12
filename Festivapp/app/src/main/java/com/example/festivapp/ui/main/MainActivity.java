944910d8dda41344343168de6c9e982a0e64a145
Carlos Moyano <carlosmoygra@gmail.com>
2020-03-12 20:11:30 +0100

    Declarada actividad de nombre MainActivity como main en el archivo manifest. Ahora el usuario puede acceder directamente al menú principal sin pasar por el login si tiene una sesión válida almacenada.

--- Festivapp/app/src/main/java/com/example/festivapp/ui/main/MainActivity.java
+++ Festivapp/app/src/main/java/com/example/festivapp/ui/main/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 import com.example.festivapp.R;
 import com.example.festivapp.ui.login.LoginActivity;
-import com.example.festivapp.ui.registro.RegistroActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -14,31 +13,52 @@ import com.parse.ParseUser;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
 
-        // Botón flotante
-        /* TODO: definir comportamiento del botón flotante */
-        FloatingActionButton fab = findViewById(R.id.fab);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser == null) {
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+            Log.d(TAG, "No hay datos del usuario almacenados, redirigiendo a login");
+
+            /* Mandamos al usuario al login */
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+
+        } else {
+
+            // El usuario no necesita pasar por LoginActivity
+
+            Log.d(TAG, "Hay datos del usuario almacenados");
+
+            setContentView(R.layout.activity_main);
+            SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+            ViewPager viewPager = findViewById(R.id.view_pager);
+            viewPager.setAdapter(sectionsPagerAdapter);
+            TabLayout tabs = findViewById(R.id.tabs);
+            tabs.setupWithViewPager(viewPager);
+
+            // Botón flotante
+            /* TODO: definir comportamiento del botón flotante */
+            FloatingActionButton fab = findViewById(R.id.fab);
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+
+        }
     }
 
     public void logout(View view) {
